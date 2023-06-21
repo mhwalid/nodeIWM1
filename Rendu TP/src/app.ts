2dac55eac4d64f8f11139f3a3f9165fe52cfc831
Basile2121 <basile.regnault@gmail.com>
2023-06-21 16:53:25 +0200

    Ajout du fichier dotenv dans le projet

--- Rendu TP/src/app.ts
+++ Rendu TP/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
 const app = express()
+
+dotenv.config();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 // Server connection
-app.listen(3000, () => console.log('Server is listening on port 3000'))
+app.listen(process.env.PORT, () => console.log('Server is listening on port 3000'))
